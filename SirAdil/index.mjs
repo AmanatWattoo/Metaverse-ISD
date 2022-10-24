e42d5b63c37faf305e25902fc453dc1de05989b1
AmanatWattoo <80233118+AmanatWattoo@users.noreply.github.com>
2022-10-24 18:00:53 +0500

    arrays concept

---
+++ SirAdil/index.mjs
@@ -0,0 +1,9 @@
+// const numArray=[50,30,60,18,19,7,13,99];
+// console.log(numArray);
+// numArray.sort();
+// console.log(numArray);
+let ages=[55,77,1,18,72,33,56,40];
+ages.sort();
+console.log(ages);
+ages.reverse();
+console.log(ages);
