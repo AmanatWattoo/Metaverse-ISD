b96880cd2711340d59455020cb71723849799de3
AmanatWattoo <80233118+AmanatWattoo@users.noreply.github.com>
2022-10-24 18:14:32 +0500

    arrays discripts

--- SirAdil/index.mjs
+++ SirAdil/index.mjs
@@ -1,9 +1,18 @@
+// Multidimensional Array
+const array1=[1,2,3,6];
+const array2=[array1,array1,4,5,9,7,8,6];
+console.log(array2);
+array2.sort();
+console.log(array2);
+console.log(array2[1][1]);
+// console.log(array2);
 // const numArray=[50,30,60,18,19,7,13,99];
 // console.log(numArray);
 // numArray.sort();
 // console.log(numArray);
-let ages=[55,77,1,18,72,33,56,40];
-ages.sort();
-console.log(ages);
-ages.reverse();
-console.log(ages);
+// let ages=[55,77,1,18,72,33,56,40];
+// ages.sort();
+// console.log(ages);
+// ages.reverse();
+// console.log(ages);
+
