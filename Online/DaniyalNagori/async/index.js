28ac01434c1c04161a62ee2b6a5143027e77b5fb
AmanatWattoo <80233118+AmanatWattoo@users.noreply.github.com>
2022-11-16 18:32:43 +0500

    updated

--- Online/DaniyalNagori/async/index.js
+++ Online/DaniyalNagori/async/index.js
@@ -1,6 +1,22 @@
-// console.log("helloo");
-console.log("First");    
+// // console.log("helloo");
+// console.log("First");    
+// setTimeout(()=>{
+//         console.log("its second")
+//     },3000);
+// console.log("Its 3rd");
+const loginuser=(email,pswd,callback)=>{
 setTimeout(()=>{
-        console.log("its second")
-    },3000);
-console.log("Its 3rd");
+    callback(email);
+},2000)
+}
+const getuserVedios= (email,cb)=> {
+    setTimeout((cb) => {
+        cb(["Vedios", "Cartton", "Scifiy"])
+    }, 3000);
+}
+loginuser("Amanat","Ali",(email)=>{
+    console.log(email)
+getuserVedios(email,(cb)=>{
+    console.log("Ready to use")
+})
+});
