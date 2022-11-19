4bb21ec9d1dbe5c12d151b3e4d35ad7017fb9d52
AmanatWattoo <80233118+AmanatWattoo@users.noreply.github.com>
2022-11-19 11:24:46 +0500

    update

--- Online/selfPractise/index.js
+++ Online/selfPractise/index.js
@@ -1,24 +1,8 @@
-function jan(weather){
-    setTimeout(function() {
-       weather("Cold") 
-    },2000)
+const userLoged=(email,passwprd,cb)=>{
+    setTimeout(() => {
+        cb("Amanat");
+    }, 2000);
 }
-function Feb(weather){
-    setTimeout(function(){
-        weather("Cool")
-    },1500)
-}
-function July(weather){
-    setTimeout (function(){
-        weather("Hot")
-    })
-}
-jan(function(weather){
-    console.log(weather);
-});
-Feb(function(weather){
-    console.log(weather);
-});
-July(function(weather){
-    console.log(weather);
-});
+userLoged("Wattoo Brother@gmail.com","123456",(name)=>{
+console.log("Logged User Is ", email);
+})
