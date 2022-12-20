9e0afcb5564e808d762230c1003abc20e4c7d568
AmanatWattoo <80233118+AmanatWattoo@users.noreply.github.com>
2022-12-20 18:10:20 +0500

    finally calculator added

---
+++ Typescript/Daniyal Nagori/ATM/index.js
@@ -0,0 +1,3 @@
+#! usr/bin/env node 
+import chalk from "chalk";
+console.log(chalk.blue("HI"));
---
+++ Typescript/mycalculator/index.js
@@ -0,0 +1,71 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalkAnimation from "chalk-animation";
+const time = (ms = 3000) => new Promise((resolve, reject) => setTimeout(resolve, ms));
+async function start() {
+    const title = chalkAnimation.rainbow("Welcome to my calculator");
+    await time();
+    title.stop();
+}
+await start();
+async function input() {
+    // input value
+    const answers = await inquirer.prompt([
+        {
+            type: "number",
+            name: "n1",
+            message: "Enter the first value : \n"
+        },
+        {
+            type: "number",
+            name: "n2",
+            message: "Enter the second value : \n"
+        },
+        {
+            type: "list",
+            name: "operator",
+            choices: ["+", "-", "*", "/"],
+            message: "Slect the operator : "
+        }
+    ]);
+    const { n1, n2, operator } = answers;
+    if (n1 && n2 && operator) {
+        let result = 0;
+        if (operator == "+") {
+            result = n1 + n2;
+            console.log(`The sum of two number is ${result}`);
+        }
+        else if (operator == "-") {
+            result = n1 - n2;
+            console.log(`The Subtraction of two number is ${result}`);
+        }
+        else if (operator == "*") {
+            result = n1 * n2;
+            console.log(`The Multiplication of two number is ${result}`);
+        }
+        else if (operator == "/") {
+            result = n1 / n2;
+            console.log(`The Division of two number is ${result}`);
+        }
+    }
+}
+async function startagin() {
+    do {
+        await input();
+        var again = await inquirer.prompt([
+            {
+                type: "input",
+                name: "restart",
+                message: "Would you like to continue press (y) or not press (any key)\n"
+            }
+        ]);
+    } while (again.restart == "y" || again.restart == "Y" || again.restart == "Yes" || again.restart == "YES");
+}
+await startagin();
+const close = (duration = 6000) => new Promise((resolve, reject) => setTimeout(resolve, duration));
+async function finish() {
+    const end = await chalkAnimation.karaoke("----------Goodbye------");
+    await close();
+    end.stop();
+}
+finish();
