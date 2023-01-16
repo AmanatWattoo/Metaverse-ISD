c90d631542b6b0aca21e6586fcd0c2aa7270cad2
AmanatWattoo <80233118+AmanatWattoo@users.noreply.github.com>
2023-01-16 23:27:55 +0500

    gout added

---
+++ Nextjs/basic/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function name(){
+  return(
+    <div>
+      <h2>Welcome to Nextjs</h2>
+    </div>
+  )
+}
---
+++ Q2/OneSite/learn/src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from './page.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div className={styles.main}>
+    <h2 className={styles.center}>Welcome to main page</h2>
+    <p className='body'>This is beging of revolution world </p>
+    </div>
+  )
+}
